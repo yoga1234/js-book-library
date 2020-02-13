2b41927fe1127e1fb3a1ca6154bf4546bca3818a
Yogasmara <septianayoga30@gmail.com>
2020-02-13 14:47:01 +0700

    add simple render function

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// card-container
+let cardContainer = document.querySelector('.card-container');
+
 // array for saving book data
-let myLibrary = [];
+let myLibrary = [
+  {
+    author: "Yogasmara",
+    title: "The Dark Project",
+    pages: 2556
+  },
+  {
+    author: "Hisako Arata",
+    title: "Good Plates",
+    pages: 299
+  }
+];
 
 function Book() {
   // the constructor
@@ -7,4 +21,34 @@ function Book() {
 
 function addBookToLibrary() {
   // do stuff here
+}
+
+// render array to HTML
+function render() {
+  myLibrary.forEach(function(currentValue, index) {
+    cardContainer.innerHTML += cardElement(currentValue)
+  })
+}
+
+// card element
+function cardElement(data) {
+  return `
+  <div class="card">
+    <div class="card-top">
+      <div class="content">
+        <div class="left-side">
+          <p>read</p>
+        </div> <!-- left-side -->
+        <div class="right-side">
+          <p class="title"><b>${data.title}</b></p>
+          <p>${data.author} - ${data.pages} pages</p>
+        </div> <!-- right-side -->
+      </div>
+    </div> <!-- card-top -->
+    <div class="card-bottom">
+      <button class="read-button"><b>read</b></button>
+      <button class="delete-button"><b>delete</b></button>
+    </div> <!-- card-bottom -->
+  </div> <!-- card -->
+  `
 }
